f4f99599e7ebb60a294fd58bd2f8ab2d485b6732
Eftekhar Mahmud <113287518+EFTY1309@users.noreply.github.com>
2023-01-28 11:33:35 +0600

    Add files via upload

---
+++ l21_norm.cpp
@@ -0,0 +1,261 @@
+#include <iostream>
+#include <math.h>
+#include "print_matrix.h"
+#include "matrix_operation.h"
+#include "matrix_factorizations.h"
+using namespace std;
+
+void update_H(double **W, double **H, double **V, int row, int k, int col)
+{
+    double *transpose_W[N], *numerator[N],*transpose_WV[N],*D[N],*WH[N];
+
+    // allocating transpose_W and numerator
+    for (int i = 0; i < k; i++)
+        transpose_W[i] = (double *)malloc(row * sizeof(double));
+
+    for (int i = 0; i < k; i++)
+        numerator[i] = (double *)malloc(col * sizeof(double));
+
+    for(int i=0;i<k;i++)
+        transpose_WV[i]=(double *)malloc(col * sizeof(double));
+
+     for(int i=0;i<row;i++)
+        D[i]=(double *)malloc(col * sizeof(double));
+
+      for(int i=0;i<row;i++)
+      WH[i]=(double *)malloc(col * sizeof(double));
+
+
+    multiply(WH,W, H, row, k, col);
+    FindD(D,V,WH,row,col);
+
+    cout<<"DH matrix is:"<<endl;
+    print_matrix(D,row,col);
+
+    transpose(W, transpose_W, row, k); // WT
+
+    multiply(transpose_WV, transpose_W, V, k, row, col);// WT*V
+    multiply(numerator, transpose_WV, D, k, col, row);// WT*VD
+
+
+    double *den_part1[N], *denominator[N],*transpose_WWH[N];
+
+    for (int i = 0; i < k; i++)
+        den_part1[i] = (double *)malloc(k * sizeof(double));
+
+     for (int i = 0; i < k; i++)
+        transpose_WWH[i] = (double *)malloc(col * sizeof(double));
+
+    for (int i = 0; i < k; i++)
+        denominator[i] = (double *)malloc(col * sizeof(double));
+
+    multiply(den_part1, transpose_W, W, k, row, k); // WT*W
+    multiply(transpose_WWH, den_part1, H, k, k, col);//(WT*W)*H
+    multiply(denominator, transpose_WWH, D, k, col, row);//(WT*W)*HD
+
+    double *updated_H[N];                           // the term that is to be multiplied with H
+
+    for (int i = 0; i < k; i++)
+        updated_H[i] = (double *)malloc(col * sizeof(double));
+    divide_element_wise(updated_H, numerator, denominator, k, col);
+
+    double *ans_H[N];
+
+    for (int i = 0; i < k; i++)
+        ans_H[i] = (double *)malloc(col * sizeof(double));
+    multiply_element_wise(ans_H, H, updated_H, k, col);
+
+    copy_matrix(ans_H, H, k, col);
+    free_matrix(ans_H, k);
+    free_matrix(updated_H, k);
+    free_matrix(denominator, k);
+    free_matrix(den_part1, k);
+    free_matrix(numerator, k);
+    free_matrix(transpose_W, k);
+
+    free_matrix(transpose_WV, k);
+    free_matrix(transpose_WWH, k);
+    free_matrix(D, row);
+    free_matrix(WH, row);
+
+
+}
+void update_W(double **W, double **H, double **V, int row, int k, int col)
+{
+    double *HT[N], *numerator[N],*VD[N],*D[N],*WH[N];
+
+    for (int i = 0; i < col; i++)
+        HT[i] = (double *)malloc(k * sizeof(double));
+
+    for (int i = 0; i < col; i++)
+        VD[i] = (double *)malloc(row * sizeof(double));
+
+    for (int i = 0; i < row; i++)
+        numerator[i] = (double *)malloc(k * sizeof(double));
+
+    for(int i=0;i<row;i++)
+        D[i]=(double *)malloc(col * sizeof(double));
+
+    for(int i=0;i<row;i++)
+        WH[i]=(double *)malloc(col * sizeof(double));
+
+
+    multiply(WH,W, H, row, k, col);
+    FindD(D,V,WH,row,col);
+
+    cout<<"DW matrix is:"<<endl;
+    print_matrix(D,row,col);
+
+    transpose(H, HT, k, col); // HT
+    multiply(VD, V, D, row, col, col); // V*HT
+    multiply(numerator, VD, HT, row, col, k); // V*HT
+
+    double *HD[N],*HD_transpose_H[N];
+
+     for (int i = 0; i < k; i++)
+        HD[i] = (double *)malloc( col * sizeof(double));
+
+     for (int i = 0; i < k; i++)
+        HD_transpose_H[i] = (double *)malloc( k * sizeof(double));
+
+    multiply(HD, H, D, k, col, row); // HD
+    multiply(HD_transpose_H, HD, HT, k, col, k); // HD_transpose_H
+
+    double *denominator[N];
+    for (int i = 0; i < row; i++)
+        denominator[i] = (double *)malloc(k * sizeof(double));
+
+    multiply(denominator, W, HD_transpose_H, row, k, k);
+
+    double *updated_W[N];
+
+    for (int i = 0; i < row; i++)
+        updated_W[i] = (double *)malloc(k * sizeof(double));
+
+    divide_element_wise(updated_W, numerator, denominator, row, k);
+
+    double *ans_W[N];
+
+    for (int i = 0; i < row; i++)
+        ans_W[i] = (double *)malloc(k * sizeof(double));
+
+    multiply_element_wise(ans_W, W, updated_W, row, k);
+
+    copy_matrix(ans_W, W, row, k);
+    free_matrix(ans_W, row);
+    free_matrix(updated_W, row);
+    free_matrix(denominator, row);
+    free_matrix(HT, col);
+    free_matrix(numerator, row);
+
+    free_matrix(VD, row);
+    free_matrix(HD, k);
+    free_matrix(HD_transpose_H, k);
+    free_matrix(D, row);
+    free_matrix(WH, row);
+}
+
+void l21_norm()
+{
+    double *matrix[N];
+    int row, col, i, j, k;
+
+    cout<<"Enter the number of row and column for matrix:"<<endl;
+    // Taking input
+    cin >> row >> col; // m*n matrix
+
+    for (i = 0; i < row; i++)
+        matrix[i] = (double *)malloc(col * sizeof(double));
+
+    cout<<"Enter matrix:"<<endl;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+            cin >> matrix[i][j];
+    }
+
+    print_matrix(matrix, row, col);
+    // Generating two matrices using user input dimension and random number generator
+    //  Dimension of broken matrix
+    cout<<"Enter the dimension in which you want to break:";
+    cin >> k;
+    double *W[N], *H[N]; // broken down in m*k and k*n matrix
+
+    for (i = 0; i < row; i++)
+        W[i] = (double *)malloc(k * sizeof(double));
+    for (i = 0; i < k; i++)
+        H[i] = (double *)malloc(col * sizeof(double));
+
+    cout<<"Enter W matrix:"<<endl;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < k; j++)
+        {
+            cin>>W[i][j];
+        }
+    }
+
+    cout<<"Enter H matrix:"<<endl;
+    for (i = 0; i < k; i++)
+    {
+        for (j = 0; j < col; j++){
+
+            cin>>H[i][j];
+        }
+    }
+
+    cout<<"Show initial two matrices";
+    print_two_matrix(W, H, row, k, col);
+    // multiplication
+    double *V[N];
+    for (i = 0; i < row; i++)
+        V[i] = (double *)malloc(col * sizeof(double));
+    multiply(V, W, H, row, k, col);
+
+    //printf("V= \n");
+
+    int counter = 1;
+    printf("Initial cost: ");
+    // cost function
+    double cost = cost_function(matrix, V, row, col);
+    double initial_cost = cost;
+    double prev_cost = 0;
+    printf("Updating costs:\n ");
+    while (cost > EPSILON)
+    {
+
+        if ((counter % 2) == 0)
+        {
+            update_H(W, H, matrix, row, k, col);
+            cout << "New H" << endl;
+        }
+        else
+        {
+            update_W(W, H, matrix, row, k, col);
+            cout << "New W" << endl;
+        }
+        counter++;
+        multiply(V, W, H, row, k, col);
+        cost = cost_function(matrix, V, row, col);
+        if (fabs(prev_cost - cost) <= EPSILON)
+        {
+            printf("Reached relative minima\n");
+            break;
+        }
+        else
+        {
+            prev_cost = cost;
+        }
+
+        // local minima reached need to stop by calculating difference with previous error
+    }
+    printf("Factorization done!");
+    //freopen("result.txt", "w", stdout);
+    printf("The beginning cost was: %lf\n", initial_cost);
+    printf("The final cost was: %lf\n", cost);
+    printf("Total number of iterations before arriving at result: %d\n", counter);
+    printf("The broken down matrix:\n ");
+    print_two_matrix(W, H, row, k, col);
+}
+
+
