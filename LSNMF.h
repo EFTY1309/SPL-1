71b7da154a328321baff97007c6c9f924e53a900
Eftekhar Mahmud <113287518+EFTY1309@users.noreply.github.com>
2023-05-25 00:49:37 +0600

    Add files via upload

---
+++ LSNMF.h
@@ -0,0 +1,335 @@
+#pragma once
+#include <bits/stdc++.h>
+#include <math.h>
+#include "print_mat.h"
+#include "matrix_operation.h"
+
+using namespace std;
+
+void update_W2(double **W, double **H, double **V, int row, int k, int col)
+{
+    double *HT[N], *numerator[N];
+
+    for (int i = 0; i < col; i++)
+        HT[i] = (double *)malloc(k * sizeof(double));
+
+
+    for (int i = 0; i < row; i++)
+        numerator[i] = (double *)malloc(k * sizeof(double));
+
+
+    //cout<<"D matrix is:"<<endl;
+    //print_matrix(D,row,col);
+
+    transpose(H, HT, k, col); // HT // V*HT
+    multiply(numerator, V, HT, row, col, k); // VHT
+
+    double *H_transpose_H[N];
+
+     for (int i = 0; i < k; i++)
+        H_transpose_H[i] = (double *)malloc( k * sizeof(double));
+      // HD
+      multiply(H_transpose_H, H, HT, k, col, k); // H_transpose_H
+
+    double *denominator[N];
+
+    for (int i = 0; i < row; i++)
+        denominator[i] = (double *)malloc(k * sizeof(double));
+
+
+    multiply(denominator, W, H_transpose_H, row, k, k);
+
+    double *updated_W[N];
+
+    for (int i = 0; i < row; i++)
+        updated_W[i] = (double *)malloc(k * sizeof(double));
+
+    divide_element_wise(updated_W, numerator, denominator, row, k);
+
+    double *ans_W[N];
+
+    for (int i = 0; i < row; i++)
+        ans_W[i] = (double *)malloc(k * sizeof(double));
+
+    multiply_element_wise(ans_W, W, updated_W, row, k);
+
+    copy_matrix(ans_W, W, row, k);
+    free_matrix(ans_W, row);
+    free_matrix(updated_W, row);
+    free_matrix(denominator, row);
+    free_matrix(HT, col);
+    free_matrix(numerator, row);
+
+    free_matrix(H_transpose_H, k);
+}
+
+void update_H2(double **W, double **H, double **V, int row, int k, int col)
+{
+double *transpose_W[N], *numerator[N];
+    // allocating transpose_W and numerator
+    for (int i = 0; i < k; i++)
+        transpose_W[i] = (double *)malloc(row * sizeof(double));
+
+    for (int i = 0; i < k; i++)
+        numerator[i] = (double *)malloc(col * sizeof(double));
+
+    transpose(W, transpose_W, row, k); // WT
+
+    multiply(numerator, transpose_W, V, k, row, col);// WT*V
+    
+    double *den_part1[N], *denominator[N];
+
+    for (int i = 0; i < k; i++)
+        den_part1[i] = (double *)malloc(k * sizeof(double));
+
+    for (int i = 0; i < k; i++)
+        denominator[i] = (double *)malloc(col * sizeof(double));
+
+    multiply(den_part1, transpose_W, W, k, row, k); // WT*W
+    multiply(denominator, den_part1, H, k, k, col);//(WT*W)*H
+    
+
+    double *updated_H[N];                           // the term that is to be multiplied with H
+
+    for (int i = 0; i < k; i++)
+        updated_H[i] = (double *)malloc(col * sizeof(double));
+    divide_element_wise(updated_H, numerator, denominator, k, col);
+
+    double *ans_H[N];
+
+    for (int i = 0; i < k; i++)
+        ans_H[i] = (double *)malloc(col * sizeof(double));
+    multiply_element_wise(ans_H, H, updated_H, k, col);
+
+    copy_matrix(ans_H, H, k, col);
+    free_matrix(ans_H, k);
+    free_matrix(updated_H, k);
+    free_matrix(denominator, k);
+    free_matrix(den_part1, k);
+    free_matrix(numerator, k);
+    free_matrix(transpose_W, k);
+
+}
+
+void LSNMF()
+{
+    double *matrix[N];
+    int row, col, i, j, k;
+
+     char choice;
+     std::srand(std::time(0));
+
+    cout << "Enter 'M' for manual input or 'F' to read from a file: ";
+    cin >> choice;
+
+    if (choice == 'M' || choice == 'm') {
+
+       cout<<"Enter the number of row and column for matrix:"<<endl;
+    // Taking input
+    cin >> row >> col; // m*n matrix
+
+    for (i = 0; i < row; i++)
+        matrix[i] = (double *)malloc(col * sizeof(double));
+
+    cout<<"Enter matrix:"<<endl;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+            cin >> matrix[i][j];
+    }
+
+    } else if (choice == 'F' || choice == 'f') {
+        ifstream inputFile("matrix.txt");
+
+        if (!inputFile.is_open()) {
+            cout << "Failed to open the file." << endl;
+            return;
+        }
+
+        // Read the number of rows and columns
+        inputFile >> row >> col;
+
+        // Allocate memory for the matrix
+        for (i = 0; i < row; i++) {
+            matrix[i] = new double[col];
+        }
+
+        // Read the matrix elements
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < col; j++) {
+                inputFile >> matrix[i][j];
+            }
+        }
+
+        inputFile.close();
+    } else {
+        cout << "Invalid choice. Exiting." << endl;
+        return;
+    }
+
+    // Rest of the code remains unchanged
+    // ...
+
+    // Deallocate memory for the matrix
+    //print_matrix(matrix, row, col);
+    //  Dimension of broken matrix
+    cout<<"Enter the dimension in which you want to break:";
+    cin >> k;
+    double *W[N], *H[N]; // broken down in m*k and k*n matrix
+
+    cout << "Enter 'M' to manually input W matrix or 'F' to read from a file: ";
+    char ch0;
+    cin>>ch0;
+
+    if (ch0 == 'M' || ch0 == 'm') {
+        cout << "Enter W matrix:" << endl;
+
+        for (i = 0; i < row; i++)
+            W[i] = new double[k];
+
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < k; j++) {
+                cin >> W[i][j];
+            }
+        }
+    }
+
+    else if (ch0 == 'F' || ch0 == 'f') {
+        ifstream wFile("W.txt");
+
+        if (!wFile.is_open()) {
+            cout << "Failed to open the W matrix file." << endl;
+            return;
+        }
+
+        // Read the number of rows and columns for W matrix
+        int wRow, wCol;
+        wFile >> wRow >> wCol;
+
+        if (wRow != row || wCol != k) {
+            cout << "Invalid dimensions for W matrix. Exiting." << endl;
+            return;
+        }
+
+        for (i = 0; i < row; i++)
+            W[i] = new double[k];
+
+        // Read the W matrix elements
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < k; j++) {
+                 wFile >> W[i][j];
+            }
+        }
+
+        wFile.close();
+
+    }
+    else {
+        cout << "Invalid choice. Exiting." << endl;
+        return;
+    }
+    cout << "Enter 'M' to manually input W matrix or 'F' to read from a file: ";
+    char ch1;
+    cin>>ch1;
+    
+    if (ch1 == 'M' || ch1 == 'm') {
+        cout << "Enter H matrix:" << endl;
+
+        for (i = 0; i < k; i++)
+            H[i] = new double[col];
+
+        for (i = 0; i < k; i++) {
+            for (j = 0; j < col; j++) {
+                cin >> H[i][j];
+            }
+        }
+
+    } else if (ch1== 'F' || ch1 == 'f') {
+        ifstream hFile("H.txt");
+
+        if (!hFile.is_open()) {
+            cout << "Failed to open the H matrix file." << endl;
+            return;
+        }
+
+        // Read the number of rows and columns for H matrix
+        int hRow, hCol;
+        hFile >> hRow >> hCol;
+
+        if (hRow != k || hCol != col) {
+            cout << "Invalid dimensions for H matrix. Exiting." << endl;
+            return;
+        }
+
+        for (i = 0; i < k; i++)
+            H[i] = new double[col];
+
+        // Read the H matrix elements
+        for (i = 0; i < k; i++) {
+            for (j = 0; j < col; j++) {
+                hFile >> H[i][j];
+            }
+        }
+
+        hFile.close();
+
+    } else {
+        cout << "Invalid choice. Exiting." << endl;
+        return;
+    }
+
+    //cout<<"Show initial two matrices";
+    //print_two_matrix(W, H, row, k, col);
+    // multiplication
+    double *V[N];
+    for (i = 0; i < row; i++)
+        V[i] = (double *)malloc(col * sizeof(double));
+
+        multiply(V, W, H, row, k, col);
+
+    //printf("V= \n");
+
+    int counter = 1;
+    printf("Initial cost: ");
+    // cost function
+    double cost = cost_function(matrix, V, row, col);
+    double initial_cost = cost;
+    double prev_cost = 0;
+    printf("Updating costs:\n ");
+    while (cost > EPSILON)
+    {
+
+        if ((counter % 2) == 0)
+        {
+            update_H2(W, H, matrix, row, k, col);
+            //cout << "New H" << endl;
+        }
+        else
+        {
+             update_W2(W, H, matrix, row, k, col);
+           // cout << "New W" << endl;
+            //print_matrix(W,row,k);
+        }
+        counter++;
+        multiply(V, W, H, row, k, col);
+        cost = cost_function(matrix, V, row, col);
+        if (fabs(prev_cost - cost) <= EPSILON)
+        {
+            printf("Reached relative minima\n");
+            break;
+        }
+        else
+        {
+            prev_cost = cost;
+        }
+
+        // local minima reached need to stop by calculating difference with previous error
+    }
+    printf("Factorization done!");
+    printf("The beginning cost was: %lf\n", initial_cost);
+    printf("The final cost was: %lf\n", cost);
+    printf("Total number of iterations before arriving at result: %d\n", counter);
+    printf("The broken down matrix:\n ");
+    print_two_matrix(W, H, row, k, col);
+}
+
--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
+
 #include <iostream>
-#include "matrix_factorizations.h"
+#include "l21_norm.h"
+#include "G_NMF.h"
+#include "RSNMF.h"
+#include "LSNMF.h"
+
+#include <windows.h>
 
 int main()
 {
+
     printf("Welcome to matrix factorization!\n");
-    printf("1. Matrix factorization using l2,1 norm");
+    printf("1. matrix factorization using Lee and Sungs Multiplicative update\n");
+    printf("2. matrix factorization using l21 norm\n");
+    printf("3. matrix factorization using Graph Laplacian Matrix\n");
+    printf("4. matrix factorization using Robust structured Non negative matrix factorization\n");
 
     int choice;
     printf("\nEnter your choice: ");
@@ -13,30 +23,26 @@ int main()
     {
         switch (choice)
         {
+
         case 1:
+            printf("You have chosen lee and Seiungs non-negative matrix factorization\n");
+            LSNMF();
+            break;
+        case 2:
             printf("You have chosen l21 norm matrix factorization\n");
             l21_norm();
+            break;
+        case 3:
+            printf("You have chosen Graph regularized Non negative matrix factorization\n");
+            G_NMF();
+            break;
+
+        case 4:
+            printf("You have chosen Robust structured Non negative matrix factorization\n");
+            RSNMF();
             break;
 
         }
     }
 }
-
-/*
-1
-5 5
-1 3 4 5 6
-1 2 4 5 1
-1 2 3 5 6
-6 7 8 9 10
-11 12 13 14 15
-3
-2 3 4
-3 4 5
-6 7 8
-9 10 11
-2 4 5
-3 4 5 6 7
-1 2 3 4 5
-2 7 8 9 10*/
 
